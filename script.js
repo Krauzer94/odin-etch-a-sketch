918a8a4e5333f6079ed1923fcfc08c7490d5c3c6
KrauzerPH94 <13894059+KrauzerPH94@users.noreply.github.com>
2023-09-29 00:01:49 -0300

    Create div JS testing

---
+++ script.js
@@ -0,0 +1,6 @@
+// Creating initial elements
+const container = document.querySelector('.container');
+const content = document.createElement('div');
+content.classList.add('content');
+content.textContent = 'Testing';
+container.appendChild(content);
