ab80669c20092557d8f4451d10543a54e6623968
KrauzerPH94 <13894059+KrauzerPH94@users.noreply.github.com>
2023-10-03 02:08:24 -0300

    Messing around with JavaScript styling

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 // Main section selector
 const container = document.querySelector('.container');
+const header = document.querySelector('.header');
+const footer = document.querySelector('.footer');
 
 // Matrix for all 16x16
 const matrix = document.createElement('div');
 matrix.classList.add('main-matrix');
 container.appendChild(matrix);
+
+// Styling using JS
+container.style.backgroundColor = "#1E1E1E";
+container.style.display = "flex";
+container.style.justifyContent = "center";
+header.style.backgroundColor = "#303030";
+footer.style.backgroundColor = "#303030";
