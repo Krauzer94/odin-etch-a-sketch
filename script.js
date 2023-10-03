48d67e66c1c1b2318344a3670ce6c6efb246a6b8
KrauzerPH94 <13894059+KrauzerPH94@users.noreply.github.com>
2023-10-03 00:15:34 -0300

    Header and footer styling on static HTML CSS

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-// Creating initial elements
+// Main section selector
 const container = document.querySelector('.container');
-const content = document.createElement('div');
-content.classList.add('content');
-content.textContent = 'Testing';
-container.appendChild(content);
+
+// Matrix for all 16x16
+const matrix = document.createElement('div');
+matrix.classList.add('main-matrix');
+container.appendChild(matrix);
