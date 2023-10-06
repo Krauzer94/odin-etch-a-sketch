a56ee085ef7e04d75c4a40c034944e9b6adaa4fd
KrauzerPH94 <13894059+KrauzerPH94@users.noreply.github.com>
2023-10-05 22:17:49 -0300

    Removed static JS styling

--- script.js
+++ script.js
@@ -7,10 +7,3 @@ const footer = document.querySelector('.footer');
 const matrix = document.createElement('div');
 matrix.classList.add('main-matrix');
 container.appendChild(matrix);
-
-// Styling using JS
-container.style.backgroundColor = "#1E1E1E";
-container.style.display = "flex";
-container.style.justifyContent = "center";
-header.style.backgroundColor = "#303030";
-footer.style.backgroundColor = "#303030";
