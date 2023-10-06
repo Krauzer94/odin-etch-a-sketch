5fcd5fff83077353ef8383ac7c2777090225b5eb
KrauzerPH94 <13894059+KrauzerPH94@users.noreply.github.com>
2023-10-06 02:15:00 -0300

    Resize main-matrix shape and border to fit 32 First internal square is shaped with 37.5px size

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
-// Main section selector
+// Main section selectors
 const container = document.querySelector('.container');
 const header = document.querySelector('.header');
 const footer = document.querySelector('.footer');
 
-// Matrix for all 16x16
+// Matrix for all 16x16 squares
 const matrix = document.createElement('div');
 matrix.classList.add('main-matrix');
 container.appendChild(matrix);
+
+// Internal squares
+const squares = document.createElement('div');
+squares.classList.add('squares');
+matrix.appendChild(squares);
+
+// Adding all 32 squares
+const temp = [];
+for (let i = 0; i <= 32; i++) {
+    temp.push(squares);
+}
+
+matrix.appendChild(temp);
