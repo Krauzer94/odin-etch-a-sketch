6b1c18b7715391fe1480153d5ff3d851e61d2778
KrauzerPH94 <13894059+KrauzerPH94@users.noreply.github.com>
2023-10-12 02:27:21 -0300

    Create 16x16 div of 256 cells

--- script.js
+++ script.js
@@ -1,35 +1,25 @@
-// Main section selectors
+// Constant tag selectors
 const container = document.querySelector('.container');
-const header = document.querySelector('.header');
-const footer = document.querySelector('.footer');
 
-// TODO: Static HTML div for testing
-const secondMatrix = document.querySelector('.second-matrix');
-
-// Matrix for all 16x16 squares
-/*
+// Central grid matrix
 const matrix = document.createElement('div');
-matrix.classList.add('main-matrix');
+matrix.classList.add('matrix');
 container.appendChild(matrix);
-*/
 
-// * Internal squares
-// const squares = document.createElement('div');
-// squares.classList.add('squares');
-// matrix.appendChild(squares);
-
-// Create a grid of variable elements
-function gridCreate(size) {
-    for (let rows = 0; rows <= size; rows++) {
+// 16x16 cells generation
+function createGrid(size) {
+    // Rows loop
+    for (let rows = 0; rows < size; rows++) {
         let cells = document.createElement('div');
-        cells.classList.add('squares');
-        secondMatrix.appendChild(cells);
-        // matrix.appendChild(squares).classList.add('squares');
-        /* for (let cols = 0; cols <= size; cols++) {
-            // Append the squares into the grid
-            // matrix.appendChild(squares).classList.add('squares');
-        } */
+        cells.classList.add('cells');
+        matrix.appendChild(cells);
+        // Columns loop
+        for (let cols = 0; cols < size - 1; cols++) {
+            let cells = document.createElement('div');
+            cells.classList.add('cells');
+            matrix.appendChild(cells);
+        }
     }
 }
 
-gridCreate(16);
+createGrid(16);
