cf29de49a8a0ff43f3e0573cf2083452692dec0c
KrauzerPH94 <13894059+KrauzerPH94@users.noreply.github.com>
2023-10-11 23:19:43 -0300

    Create 16 divs forming a column

--- script.js
+++ script.js
@@ -3,23 +3,33 @@ const container = document.querySelector('.container');
 const header = document.querySelector('.header');
 const footer = document.querySelector('.footer');
 
+// TODO: Static HTML div for testing
+const secondMatrix = document.querySelector('.second-matrix');
+
 // Matrix for all 16x16 squares
+/*
 const matrix = document.createElement('div');
 matrix.classList.add('main-matrix');
 container.appendChild(matrix);
+*/
 
-// Internal squares
-const squares = document.createElement('div');
+// * Internal squares
+// const squares = document.createElement('div');
 // squares.classList.add('squares');
 // matrix.appendChild(squares);
 
 // Create a grid of variable elements
 function gridCreate(size) {
     for (let rows = 0; rows <= size; rows++) {
-        for (let cols = 0; cols <= size; cols++) {
+        let cells = document.createElement('div');
+        cells.classList.add('squares');
+        secondMatrix.appendChild(cells);
+        // matrix.appendChild(squares).classList.add('squares');
+        /* for (let cols = 0; cols <= size; cols++) {
             // Append the squares into the grid
-        }
+            // matrix.appendChild(squares).classList.add('squares');
+        } */
     }
 }
 
-// gridCreate(16);
+gridCreate(16);
